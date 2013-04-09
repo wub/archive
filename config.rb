06aa7b44b56e331ad795934ddda672292d623e97
Jarrod Mosen <jarrod.mosen@me.com>
2013-04-09 16:13:04 +1200

    Big dump

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'zurb-foundation'
+
+# http_path = Root of deployed project
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# Line comments display the original location of your selectors.
+line_comments = false
